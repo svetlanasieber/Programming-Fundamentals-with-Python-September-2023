4101fc99ae026e5d6a31f06ffbd463b7b5ed0d01
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2023-09-21 20:47:07 +0200

    Update 10_gladiator_expenses_test.py
    n8

--- 10_gladiator_expenses_test.py
+++ 10_gladiator_expenses_test.py
@@ -1,16 +1,15 @@
 def calculate_gladiator_expenses():
-    # Input data
+
     lost_fights = int(input())
     helmet_price = float(input())
     sword_price = float(input())
     shield_price = float(input())
     armor_price = float(input())
 
-    # Initialize variables
+
     expenses = 0.0
     shield_breaks = 0
 
-    # Loop through each lost fight
     for fight in range(1, lost_fights + 1):
         helmet_broken = False
         sword_broken = False
@@ -22,17 +21,15 @@ def calculate_gladiator_expenses():
             expenses += sword_price
             sword_broken = True
 
-        # shield breaks when both sword and helmet break in the same fight
         if helmet_broken and sword_broken:
             expenses += shield_price
             shield_breaks += 1
 
-        # armor needs repair every 2nd time the shield breaks
+        
         if shield_breaks % 2 == 0 and shield_breaks != 0 and helmet_broken and sword_broken:
             expenses += armor_price
 
-    # Print the result
+   
     print(f"Gladiator expenses: {expenses:.2f} aureus")
 
-# Execute the function
 calculate_gladiator_expenses()

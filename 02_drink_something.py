7bda7ff73ac5b94aad43db0123c56f28d04d3c9c
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2023-09-21 20:49:44 +0200

    Update 02_drink_something.py
    c2

--- 02_drink_something.py
+++ 02_drink_something.py
@@ -1,14 +1,4 @@
-# A kid is defined as someone under or at the age of 14.
-# A teen is defined as someone under or at the age of 18.
-# A young adult is defined as someone under or at the age of 21.
-# An adult is defined as someone above the age of 21.
 
-# Kids drink toddy,
-# teens drink coke,
-# young adults drink beer,
-# and adults drink whisky.
-# => Create a program that
-# receives a person's age and prints what he/she drinks.
 
 ages = int(input())
 drink = ""

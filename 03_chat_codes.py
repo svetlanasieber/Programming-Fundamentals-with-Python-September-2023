7eccd356a606818f906c0e8b9ca4d34160addaae
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2023-09-21 20:50:08 +0200

    Update 03_chat_codes.py
    b5

--- 03_chat_codes.py
+++ 03_chat_codes.py
@@ -1,11 +1,4 @@
-# Create a program that receives the n number of messages sent.
-# On the following n lines, it will receive integer
-# numbers. For each number, the program should print a different message:
 
-# • If the number is 88 - "Hello"
-# • If the number is 86 - "How are you?"
-#  If the number is not 88 or 86, and it is below 88 - "GREAT!"
-# • If the number is over 88 - "Bye."
 
 #number_of_messages = int(input())
 #for current_message in range(number_of_messages):
@@ -19,7 +12,7 @@
 #    else: # elif number > 88
 #        print("Bye.")
 
-# Reshenie Ivan Shopov
+# 2.
 
 number_of_messages = int(input())
 for current_message in range(number_of_messages):

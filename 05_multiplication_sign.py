a537d2bf679f46daf4a68ab84e95ad495fa99397
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2023-09-22 11:51:29 +0200

    Add files via upload

---
+++ 05_multiplication_sign.py
@@ -0,0 +1,21 @@
+def multiplication(a, b, c):
+    result = None
+    if (c > 0 and a < 0 and b < 0) or \
+            (b > 0 and a < 0 and c < 0) or \
+            (a > 0 and b < 0 and c < 0) or \
+            (a > 0 and b > 0 and c > 0):
+        result = "positive"
+
+    elif a == 0 or b == 0 or c == 0:
+        result = "zero"
+
+    elif a < 0 or b < 0 or c < 0:
+        result = "negative"
+
+    return result
+
+
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+print(multiplication(first_number, second_number, third_number))
